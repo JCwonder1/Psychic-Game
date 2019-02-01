70325e5bfbfa0d07b2f11ae10c27b1469e8bb1d3
Joseph Cannova <jcannova@wheelership.com>
2019-02-01 17:16:38 -0500

    User input displays on screen and h3 tag changes visiblity once key is hit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,16 @@
 var body =  document.querySelector("body");
+var guessed = document.querySelector(".guessed");
+var hide = document.querySelector(".hide");
+hide.style.visibility = "hidden";
+var g;
 
+document.onkeyup = function(event) {
+    hide.style.visibility = "visible";
+    var g = (event.key);
+    var addH3 = document.createElement("h3");
+    addH3.innerHTML = g;
+    guessed.appendChild(addH3); 
 
-body.addEventListener('keyup', function(e){
-        var guessed = document.querySelector(".guessed");
-        guessed.push(e.key);
-        guessed.innerHTML = "You guessed: " + e.key;
+  };
+console.log
 
-},false);
